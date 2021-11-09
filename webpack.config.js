9e63452c7d7ffe5b2ffd1f64917293f20587a740
leeandri <rl.tsirih@gmail.com>
2021-11-10 00:00:45 +0300

    code push forgotten

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    }, 
+    devServer: {
+        contentBase: './dist',
+        open: 'chrome' 
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'        
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+                
+            }
+        ]
+    }
+};
